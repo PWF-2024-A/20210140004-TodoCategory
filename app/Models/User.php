c950afba0cc10aa0f6c5d04b7c5a91156e112716
M S I <ghani.zumar.ft21@mail.umy.ac.id>
2024-06-08 15:45:35 +0700

    api : auth, read, search data

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 //use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 
@@ -43,6 +42,7 @@ class User extends Authenticatable implements MustVerifyEmail
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'is_admin' => 'boolean'
     ];
     public function todos()
     {
---
+++ tugas11.md
@@ -0,0 +1,29 @@
+# Tugas 11
+
+## 1. Login
+
+![Alt text](screenshot/tugas11/111.png)
+
+## 2. Profile
+
+![Alt text](screenshot/tugas11/112.png)
+
+## 3. Todo Index
+
+![Alt text](screenshot/tugas11/113.png)
+
+## 4. Todo Show
+
+![Alt text](screenshot/tugas11/114.png)
+
+## 5. Todo Show Forbidden
+
+![Alt text](screenshot/tugas11/115.png)
+
+## 6. Todo Search
+
+![Alt text](screenshot/tugas11/116.png)
+
+## 7. Logout
+
+![Alt text](screenshot/tugas11/117.png)
